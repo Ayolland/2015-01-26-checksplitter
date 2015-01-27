5e9215e369b665af2d26f54c25d378362636334a
Andrew Yolland <and.t.yoll@gmail.com>
2015-01-27 10:16:08 -0600

    Separated math into separate methods. Added Attr_Reader for initial instant attributes.

--- checksplitter.rb
+++ checksplitter.rb
@@ -3,11 +3,24 @@ require 'pry'
 class Checksplitter
   def initialize(prmtotal,prmguests)
     ##This class creates an object to assist in evenly splitting a check between a number of people. When defining a checksplitter object, input the bill total and the number of guests to split it between.
-    @total = prmtotal
+    @total = prmtotal.round(2)
     @guests = prmguests.round
     @grat = 0.2
-    @eachsub = @total / @guests
     puts "You will be splitting $" + @total.to_s + " evenly between " + @guests.to_s + " people."
+  end
+  
+  attr_reader :total, :guests, :grat
+  
+  def eachsub
+    (@total / @guests).round(2)
+  end
+  
+  def tiptotal
+    total * grat
+  end
+  
+  def tipeach
+    tipeach = (tiptotal / guests).round(2)
   end
   
   def set_grat(t)
@@ -20,14 +33,12 @@ class Checksplitter
   end
   
   def split
-    tiptotal = @total * @grat
-    tipeach = (tiptotal / @guests).round(2)
-    eachsub = (@eachsub.round(2))
     puts "Each guest should pay $" + eachsub.to_s + " to the balance, and $" + tipeach.to_s + " in gratuity,"
-    puts "for a total of $" + (eachsub + tipeach).round(2).to_s + "."
+    puts "for a total of $" + (eachsub + tipeach).to_s + "."
   end
   
-  binding.pry
-  
 end
+
+
+binding.pry
     
