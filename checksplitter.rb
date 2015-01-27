d4f11728fcd5cbb7a02468493fe370ed877b679f
Andrew Yolland <and.t.yoll@gmail.com>
2015-01-27 11:17:41 -0600

    Commented out Front end to add Dinner Club class

--- checksplitter.rb
+++ checksplitter.rb
@@ -6,7 +6,7 @@ class Checksplitter
     @total = prmtotal.round(2)
     @guests = prmguests.round
     @grat = 0.2
-    puts "You will be splitting $" + @total.to_s + " evenly between " + @guests.to_s + " people."
+    #puts "You will be splitting $" + @total.to_s + " evenly between " + @guests.to_s + " people."
   end
   
   attr_reader :total, :guests, :grat
@@ -25,16 +25,17 @@ class Checksplitter
   
   def set_grat(t)
     @grat = t / 100
-    if @grat < 0.15
-      puts "That bad, huh?"
+#    if @grat < 0.15
+#      puts "That bad, huh?"
     end
-    puts "You have set a gratuity of %" + t.to_s + "." 
-    puts "Please remember: 20% is considered customary for good service."
+#    puts "You have set a gratuity of %" + t.to_s + "." 
+#    puts "Please remember: 20% is considered customary for good service."
   end
   
   def split
-    puts "Each guest should pay $" + eachsub.to_s + " to the balance, and $" + tipeach.to_s + " in gratuity,"
-    puts "for a total of $" + (eachsub + tipeach).to_s + "."
+  #  puts "Each guest should pay $" + eachsub.to_s + " to the balance, and $" + tipeach.to_s + " in gratuity,"
+  #  puts "for a total of $" + (eachsub + tipeach).to_s + "."
+  eachsub + tipeach
   end
   
 end
