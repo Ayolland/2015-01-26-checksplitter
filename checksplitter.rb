630963223f85a23149492a0f38045d973a095adc
Andrew Yolland <and.t.yoll@gmail.com>
2015-01-27 15:17:11 -0600

    Saving to access old commit

--- checksplitter.rb
+++ checksplitter.rb
@@ -1,9 +1,28 @@
 require 'pry'
 
-class Checksplitter
+class Check
 end
 
+# Class: DinnerClub
+#
+# Attributes:
+# @roster - Hash: Keys are Diner's names. Values is the total
+#                 money they have paid over all events.
+# @log    - Hash: Keys are the date of the event, Values are
+#                 Check object for that event.
 class DinnerClub
+  
+  def initialize
+    @roster = {}
+    @log ={}
+  end
+  
+  def add_member_to_club()
+    
+  end
+end
+
+class Diner
 end
 
 
