0a8cf8f72aabe57511d072fa14ccc83cd398a372
Andrew Yolland <and.t.yoll@gmail.com>
2015-01-27 13:12:12 -0600

    trying to add a log of each dinner settled, broke it in the process, forgot to commit it when it was working....

--- checksplitter.rb
+++ checksplitter.rb
@@ -2,20 +2,22 @@ require 'pry'
 
 class Checksplitter
   def initialize(param_total,dinner_club_name)
-    ##This class creates an object to assist in evenly splitting a check between a number of people. When defining a checksplitter object, input the bill total and the number of guests to split it between.
     @total = param_total.round(2)
     @guests = 0
     @members = []
     @grat = 0.2
     @club = dinner_club_name
+    @settled = :no
     #puts "You will be splitting $" + @total.to_s + " evenly between " + @guests.to_s + " people."
   end
   
-  attr_reader :total, :guests, :grat, :members, :club, :split
+  attr_reader :total, :guests, :grat, :members, :club, :split, :tipeach, :tiptotal, :settled
   
   def addguest(member_name)
+    while settle == :no
     @members.push(member_name)
     @guests += 1
+    end
   end
   
   def eachsub
@@ -27,40 +29,43 @@ class Checksplitter
   end
   
   def tipeach
-  (tiptotal / guests).round(2)
+    (tiptotal / guests).round(2)
   end
   
   def set_grat(t)
-    @grat = t / 100
-  #  if @grat < 0.15
-  #  puts "That bad, huh?"
-  #  puts "You have set a gratuity of %" + t.to_s + "." 
-  #  puts "Please remember: 20% is considered customary for good service."
+    while settle == :no
+    @grat = t * 0.01
+    end
   end
   
   def split
-  #  puts "Each guest should pay $" + eachsub.to_s + " to the balance, and $" + tipeach.to_s + " in gratuity,"
-  #  puts "for a total of $" + (eachsub + tipeach).to_s + "."
-  eachsub + tipeach
+    eachsub + tipeach
   end
   
   def settle
     @members.each do |member_name|
       @club.roster[member_name] += split
+    end
+    @settled = :yes
   end
-end
 end
 
 class DinnerClub
   
   def initialize
   @roster = {}
+  @log ={}
   end
   
   attr_reader :roster
   
   def add_member(member_name)
     @roster[member_name] = 0.0
+  end
+  
+  def settle_and_log(checksplitter_name,date)
+    checksplitter_name.settle
+    @log[date] = checksplitter_name
   end
   
 end
@@ -71,13 +76,14 @@ coolkids.add_member('Diane')
 coolkids.add_member('Ross')
 coolkids.add_member('Fozzy')
 puts coolkids.roster
-Thai = Checksplitter.new(200,coolkids)
-Thai.addguest('Diane')
-Thai.addguest('Ross')
-puts Thai.members
-Thai.set_grat(26)
-Thai.settle
+thai = Checksplitter.new(200,coolkids)
+thai.addguest('Diane')
+thai.addguest('Ross')
+puts thai.members
+thai.set_grat(26)
+coolkids.settle_and_log(thai,'1-30-2015')
 puts coolkids.roster
+coolkids.log
 
 binding.pry
     
