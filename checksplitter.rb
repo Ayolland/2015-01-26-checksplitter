97e099e6a979dbb9a08fe5cfce6f1a6d6d21c761
Andrew Yolland <and.t.yoll@gmail.com>
2015-01-27 12:42:54 -0600

    DinnerClub added, changed Checksplitter to include guests, but total no longer works.

--- checksplitter.rb
+++ checksplitter.rb
@@ -1,35 +1,41 @@
 require 'pry'
 
 class Checksplitter
-  def initialize(prmtotal,prmguests)
+  def initialize(param_total,dinner_club_name)
     ##This class creates an object to assist in evenly splitting a check between a number of people. When defining a checksplitter object, input the bill total and the number of guests to split it between.
-    @total = prmtotal.round(2)
-    @guests = prmguests.round
+    @total = param_total.round(2)
+    @guests = 0
+    @members = []
     @grat = 0.2
+    @club = dinner_club_name
     #puts "You will be splitting $" + @total.to_s + " evenly between " + @guests.to_s + " people."
   end
   
-  attr_reader :total, :guests, :grat
+  attr_reader :total, :guests, :grat, :members, :club, :split
+  
+  def addguest(member_name)
+    @members.push(member_name)
+    @guests += 1
+  end
   
   def eachsub
     (@total / @guests).round(2)
   end
   
   def tiptotal
-    total * grat
+    @total * @grat
   end
   
   def tipeach
-    tipeach = (tiptotal / guests).round(2)
+  (tiptotal / guests).round(2)
   end
   
   def set_grat(t)
     @grat = t / 100
-#    if @grat < 0.15
-#      puts "That bad, huh?"
-    end
-#    puts "You have set a gratuity of %" + t.to_s + "." 
-#    puts "Please remember: 20% is considered customary for good service."
+  #  if @grat < 0.15
+  #  puts "That bad, huh?"
+  #  puts "You have set a gratuity of %" + t.to_s + "." 
+  #  puts "Please remember: 20% is considered customary for good service."
   end
   
   def split
@@ -38,8 +44,40 @@ class Checksplitter
   eachsub + tipeach
   end
   
+  def settle
+    @members.each do |member_name|
+      @club.roster[member_name] += split
+  end
+end
 end
 
+class DinnerClub
+  
+  def initialize
+  @roster = {}
+  end
+  
+  attr_reader :roster
+  
+  def add_member(member_name)
+    @roster[member_name] = 0.0
+  end
+  
+end
+
+
+coolkids = DinnerClub.new
+coolkids.add_member('Diane')
+coolkids.add_member('Ross')
+coolkids.add_member('Fozzy')
+puts coolkids.roster
+Thai = Checksplitter.new(200,coolkids)
+Thai.addguest('Diane')
+Thai.addguest('Ross')
+puts Thai.members
+Thai.set_grat(26)
+Thai.settle
+puts coolkids.roster
 
 binding.pry
     
