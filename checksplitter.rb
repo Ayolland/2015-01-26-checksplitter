b62a8d8a2e600b3d9bbfce4a9bca5dc7f87acc51
Andrew Yolland <and.t.yoll@gmail.com>
2015-01-27 19:27:25 -0600

    beginning to add methods of splitting checks

--- checksplitter.rb
+++ checksplitter.rb
@@ -2,12 +2,12 @@ require 'pry'
 
 class Check
   
-  attr_reader :total_after_tax, :members_attending, :members_share, :number_of_guests
-  attr_accessor :gratuity, :settled
+  attr_reader :total_after_tax, :members_attending, :members_share
+  attr_accessor :gratuity, :settled, :number_of_guests
   
   def initialize(total_after_tax_temp)
     @total_after_tax = total_after_tax_temp
-    @members_attending = {} #this should be pushed from the DiningClub object
+    @members_attending = {} #this should be pushed from the DiningClub class
     @members_share = {} #this will be show what percentage of the bill/tip each member should cover
     @settled = :no #This marks the check as currently active and able to be modified.
     @number_of_guests = 0 #this will be added to as the DiningClub object adds members
@@ -16,17 +16,36 @@ class Check
   
   ##FINISH THIS DOC LATER
   #This method will take a 0-100 value and covert it to a percentage, with a minimum of .1
-  def set_gratuity
+  def set_gratuity(tip)
+    gratuity = tip * 0.01
+    gratuity = 0.01 if gratuity < 0.01
+  end
+  
+  #FINISH THIS DOC LATER
+  #
+  def split_individually
+    total_pre_tax = 0.0
+    @members_attending.each do |member_name, each_pre_tax|
+     total_pre_tax += each_pre_tax
+    end
+    @members_attending.each do |member_name, each_pre_tax|
+    @members_share[member_name] = (each_pre_tax / total_pre_tax).round(2)
+    #puts @members_share[member_name]
+    end
+    #return total_pre_tax
+  end
+  
+  ##FINISH THIS DOC LATER
+  #THis method will split the bill evenly, regardless of what each person ordered.
+  def split_evenly
+    @members_attending.each do|member_name, each_pre_tax|
+      
+    end
   end
   
   #FINISH THIS DOC LATER
   #This method will set one member's share to 100% and all others to 0%
-  def check_is_on(very_nice_person)
-  end
-  
-  ##FINISH THIS DOC LATER
-  #This method will set one member's share to be the balance not paid by other members.
-  def balance_is_on(probably_the_boss)
+  def all_on(very_nice_person)
   end
     
   #FINISH THIS DOC LATER
@@ -63,6 +82,7 @@ class DinnerClub
   def add_member_to_check(name_of_check,member_name,individual_amount_pretax)
     if name_of_check.settled == :no
     name_of_check.members_attending[member_name] = individual_amount_pretax
+    name_of_check.number_of_guests += 1
     end
   end
   
@@ -98,6 +118,8 @@ superpals = DinnerClub.new
 superpals.add_member_to_club('Batman')
 pizza = Check.new(350.55)
 superpals.add_member_to_check(pizza,'Batman',5.50)
+superpals.add_member_to_check(pizza,'Sally',10.0)
+superpals.add_member_to_check(pizza,'Mom',8.92)
 superpals.settle_check(pizza)
 superpals.add_check_to_log(pizza,'now')
 binding.pry
